2337da5e5b3250e6562764126124b71e6f183348
LiamDMC <60507779+LiamDMC@users.noreply.github.com>
2023-05-30 21:02:40 +0100

    CurrentState

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -88,18 +88,18 @@ function cartDisplay(){
   
       return (
         <div className="h-screen">
-        <p onClick={cartDisplay}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 cursor-pointer float-right mr-12 mt-4 absolute top-0 right-0">
+    
+<div className="relative">
+
+<p onClick={cartDisplay}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 cursor-pointer float-right mr-12 mt-4 absolute top-0 right-0">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
 </svg>
 </p>
-<div className="relative mt-12">
 
-
-<Link to={"/"} className="absolute left-12 top-4 mb-12 text-black " >Back &#62;</Link>
 {displayCart ? <DisplayCart displayCart={displayCart} SetDisplayCart={SetDisplayCart}/> : null}
-        <div className="grid grid-cols-2 gap-2 p-12">
+        <div className="grid grid-cols-2 gap-2 p-12 pt-20">
         <div className="col-span-1">
-        
+        <Link to={"/"} className="absolute left-12 top-4 mb-12 text-black " >Back &#62;</Link>
       <img src={data.featuredImage.url}></img>
         </div>
 
